938ddc1236cb3c32d7dfbe03d9492c5fa7409df6
garyGemvary <gary.huang@gemvary.com>
2021-11-02 13:50:31 +0800

    revise function with default port para

--- DigitIndoor/DigitIndoor.py
+++ DigitIndoor/DigitIndoor.py
@@ -4,31 +4,44 @@ import json
 import re
 import socket
 import subprocess
+from time import sleep
 
 
 class DigitIndoor:
+    """
+    library for digit indoor to answer the call or
+    """
     def __init__(self):
         pass
 
-    def start(self, indoor_ip, indoor_ip_port, case_name, case_id):
+    def start(self, indoor_ip, case_name, case_id, indoor_ip_port=8010):
         """
-        start the test case
+        the test case need to be started with ip, case_name, and test case id
         """
         msg = json.dumps({"cmd": "AutoTest", "data": {"command": "start", "caseName": case_name,
                                                       "sid": case_id}})
         socket_port = (indoor_ip, indoor_ip_port)
         self._send_message(msg, socket_port)
 
-    def monitor(self, indoor_ip, indoor_ip_port, outdoor_ip):
+    def end(self, indoor_ip, case_name, case_id, indoor_ip_port=8010):
         """
-        monitoring api for digital indoor monitor the outdoor, the default port should be 8010
+        the test case need to end with ip, case_name, and test case id
+        """
+        msg = json.dumps({"cmd": "AutoTest", "data": {"command": "end", "caseName": case_name,
+                                                      "sid": case_id}})
+        socket_port = (indoor_ip, indoor_ip_port)
+        self._send_message(msg, socket_port)
+
+    def monitor(self, indoor_ip, outdoor_ip, indoor_ip_port=8010):
+        """
+        monitoring api for digital indoor monitor the outdoor, the default port is 8010
         """
         msg = json.dumps({"cmd": "AutoTest", "data": {"command": "monitoring", "device_ip": outdoor_ip,
                                                       "device_name": "AutoTest"}})
         socket_port = (indoor_ip, indoor_ip_port)
         self._send_message(msg, socket_port)
 
-    def unlock(self, indoor_ip, indoor_ip_port):
+    def unlock(self, indoor_ip, indoor_ip_port=8010):
         """
         use the api to send unlock command, the default port should be 8010
         """
@@ -44,5 +57,8 @@ class DigitIndoor:
 
 if __name__ == '__main__':
     my = DigitIndoor()
-    my.start("192.192.255.170", 8010, "test_rf", "2")
-    my.monitor("192.192.255.170", 8010, "192.192.6.6")
+    my.start("192.192.255.170", "test_rf", "4")
+    my.monitor("192.192.255.170", "192.192.6.6")
+    sleep(5)
+    my.unlock("192.192.255.170")
+    my.end("192.192.255.170", "test_rf", "3")
